9bb5c8463b385cbac1a12700edae512bd6d7954e
anikimmel <99368888+anikimmel@users.noreply.github.com>
2024-07-06 14:50:53 -0400

    Update dtwgpu.py again removed autoinit

--- linmdtw/dtwgpu.py
+++ linmdtw/dtwgpu.py
@@ -10,6 +10,7 @@ import warnings
 DTW_Step_ = None
 DTW_GPU_Initialized = False
 DTW_GPU_Failed = False
+ctx = None
 
 def init_gpu():
     s = """
@@ -124,8 +125,11 @@ def init_gpu():
     global DTW_GPU_Initialized
     if not DTW_GPU_Initialized:
         try:
-            import pycuda.autoinit
+            import pycuda.driver as cuda
             from pycuda.compiler import SourceModule
+            cuda.init() 
+            device = cuda.Device(0) 
+            ctx = device.make_context() 
             mod = SourceModule(s)
             global DTW_Step_
             DTW_Step_ = mod.get_function("DTW_Diag_Step")
@@ -236,4 +240,5 @@ def dtw_diag_gpu(X, Y, k_save = -1, k_stop = -1, box = None, reverse=False, debu
         res['L'] = L.get()
         res['UL'] = UL.get()
         res['S'] = S.get()
+    ctx.pop() 
     return res
